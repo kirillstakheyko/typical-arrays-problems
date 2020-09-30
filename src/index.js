c19cd8099dec0fec6e955eb40e631cd2641a75fa
kirill.stakheyko <kirill.stakheyko@gmail.com>
2020-10-01 00:35:01 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
+
 exports.min = function min (array) {
-  return 0;
+  if(array){
+  let minVal = 0;
+  for (let i = 0; i < array.length; i++){
+    if(array[i] < minVal) minVal = array[i];
+  }
+  return minVal;
+} else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array){
+  let maxVal = 0;
+  for (let i = 0; i < array.length; i++){
+    if(array[i] > maxVal) maxVal = array[i];
+  }
+  return maxVal;
+} else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array && array.length !==0){
+  let sum = 0
+  let avgVal = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+  avgVal = sum / array.length;
+  return avgVal;
+} else return 0;
 }
